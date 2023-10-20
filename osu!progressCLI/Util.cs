02b880bd6b16d342167ca1929de9a3fe9b682b0c
JuLi0n21 <julianschlueterbusiness21@gmail.com>
2023-10-20 14:39:42 +0200

    Deepclone (ugly but seems to be working)

---
+++ osu!progressCLI/Util.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+
+public class Copy
+{
+    public static T DeepCopy<T>(T input)
+    {
+        var jsonString = JsonSerializer.Serialize(input);
+        return JsonSerializer.Deserialize<T>(jsonString);
+    }
+}
