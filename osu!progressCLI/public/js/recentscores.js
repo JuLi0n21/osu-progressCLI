7f22d197a27e221877bed75d4b19c21cb579e763
JuLi0n21 <julianschlueterbusiness21@gmail.com>
2023-10-31 20:22:43 +0100

    going back to CoverList display

--- osu!progressCLI/public/js/recentscores.js
+++ osu!progressCLI/public/js/recentscores.js
@@ -11,14 +11,6 @@ const scoresContainer =  document.getElementById("scorecontainer");
     const scoreElement = document.createElement("a");
     scoreElement.className = "flex justify-center mb-0";
 
-      let bg = score.Cover;
-      if (score.Cover == "null") {
-          console.log(score.Background, score.Cover)
-
-          bg = `/${score.Foldername}/${score.Background}`;
-          console.log(bg);
-      }
-
       score.Acc = score.Acc.toFixed(2);
 
       scoreElement.href = `/score.html?id=${score.id}`;
@@ -40,7 +32,7 @@ const scoresContainer =  document.getElementById("scorecontainer");
             </div>
         </div>
         <div class="backdrop--dark icon rounded-lg flex-nowrap">
-            <img src="${bg}" class="w-16 h-16" alt="?">
+            <img src="${score.Cover}" class="w-16 h-16" alt="?">
         </div>
 
         <!-- Name, Score/Combo, Grade Date -->
