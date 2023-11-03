cebade5cd46b38ca861c1bfeb8e3ca92c115978d
JuLi0n21 <julianschlueterbusiness21@gmail.com>
2023-11-03 04:51:36 +0100

    bases for Score Imports (beatmap downloading)

--- osu!progressCLI/ApiController.cs
+++ osu!progressCLI/ApiController.cs
@@ -1,7 +1,11 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using OsuMemoryDataProvider.OsuMemoryModels.Abstract;
+using System.Data.Entity.Core.Mapping;
+using System.Net;
+using System.Net.Http;
 using System.Net.Http.Headers;
-
+using System.IO.Compression;
 
 namespace osu_progressCLI
 {
@@ -67,9 +71,9 @@ namespace osu_progressCLI
                 var responseContent = await response.Content.ReadAsStringAsync();
 
                 TokenResponse tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(responseContent);
-
                 Credentials.Instance.SetAccessToken(tokenResponse.access_token);
 
+               await getMostPlayedMaps("14100399",10150,50, true);
             }
             else
             {
@@ -185,7 +189,7 @@ namespace osu_progressCLI
                     Logger.Log(Logger.Severity.Info, Logger.Framework.Network, $"Recieved User info for: {userid}");
 
                     string responseBody = await reponse.Content.ReadAsStringAsync();
-                    //Console.WriteLine(responseBody);
+                    Console.WriteLine(responseBody);
 
                     usercache = JObject.Parse(responseBody);
                 }
@@ -204,6 +208,112 @@ namespace osu_progressCLI
                 return usercache;
             }
             return usercache;
+        }
+
+        private async Task<JArray> getMostPlayedMaps(string userid, int offset = 0, int count = 1, bool downloadmissingbeatmaps = false) {
+            
+            Logger.Log(Logger.Severity.Debug, Logger.Framework.Misc, $"Requesting MostplayedMaps for: {userid} offset: {offset}, count: {count}, download?={downloadmissingbeatmaps}");
+            JArray beatmaps = null;
+
+            string MostPlayedEndpoint = $"https://osu.ppy.sh/api/v2/users/{userid}/beatmapsets/most_played?limit={count}&offset={offset}";
+            Logger.Log(Logger.Severity.Debug, Logger.Framework.Misc, $"Request {MostPlayedEndpoint}");
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri(MostPlayedEndpoint);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Credentials.Instance.GetAccessToken());
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            HttpResponseMessage reponse = await client.GetAsync(MostPlayedEndpoint);
+
+
+                if (reponse.IsSuccessStatusCode)
+                {
+
+                    string responseBody = await reponse.Content.ReadAsStringAsync();
+                    //Console.WriteLine(responseBody);
+
+                beatmaps = JArray.Parse(responseBody);
+                Logger.Log(Logger.Severity.Info, Logger.Framework.Network, $"Recieved Mostplayed for: {userid} Count:{beatmaps.Count}");
+
+                if (downloadmissingbeatmaps)
+                    {
+                        foreach (JObject beatmap in beatmaps)
+                        {
+                            await DownloadBeatmapset(int.Parse(beatmap["beatmap"]["beatmapset_id"].ToString()));
+                        }
+                     Logger.Log(Logger.Severity.Warning, Logger.Framework.Network, $"Successfully Downloaded All Requested Beatmaps");
+
+                    }
+                }
+                else
+                {
+                Logger.Log(Logger.Severity.Warning, Logger.Framework.Network, $"User Request failed with status code {reponse.StatusCode}");
+
+                client.Dispose();
+                    return beatmaps;
+                }
+                
+                client.Dispose();
+                return beatmaps;
+        }
+
+        private async Task<bool> DownloadBeatmapset(int beatmapsetid) {
+            Logger.Log(Logger.Severity.Info, Logger.Framework.Network, $"Checking if Beatmapset Exists: {beatmapsetid}");
+
+            string[] files = Directory.GetDirectories(Credentials.Instance.GetConfig().songfolder);
+            
+            bool doesexist = false;
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i].Contains($"{beatmapsetid} ")) //avoids 2345 counting for 234 
+                {
+                    Logger.Log(Logger.Severity.Info, Logger.Framework.Network, $"Beatmap Set with ID: {beatmapsetid} Exists! Canceling Download...");
+
+                    return false;
+                }
+            }
+
+
+            if (!doesexist)
+            {
+                {
+                    try {
+                        Logger.Log(Logger.Severity.Info, Logger.Framework.Network, $"Starting Download for:  {beatmapsetid}");
+
+                        using HttpClient client = new HttpClient();
+
+                        byte[] mapFile = client.GetByteArrayAsync($"https://api.chimu.moe/v1/download/{beatmapsetid}").Result;
+                        string mapNameJson = client.GetStringAsync($"https://api.chimu.moe/v1/set/{beatmapsetid}").Result;
+                        dynamic mapNameData = JsonConvert.DeserializeObject(mapNameJson);
+                        // Console.WriteLine(mapNameJson);
+                        string artist = mapNameData.Artist;
+                        string title = mapNameData.Title;
+
+                        string sanitizedMapName = $"{mapNameData.SetId} {artist} - {title}";
+
+                        string filePath = Path.Combine(Credentials.Instance.GetConfig().songfolder, $"{sanitizedMapName}.osz");
+                        string dirPath = Path.Combine(Credentials.Instance.GetConfig().songfolder, $"{sanitizedMapName}");
+
+
+                        using (FileStream fileStream = File.OpenWrite(filePath))
+                        {
+                            fileStream.Write(mapFile, 0, mapFile.Length);
+                        }
+
+                        ZipFile.ExtractToDirectory(filePath, dirPath);
+
+                        Logger.Log(Logger.Severity.Info, Logger.Framework.Network, $"Beatmap Set with ID: {beatmapsetid} Downloaded Succesfully");
+                    } catch (Exception ex)
+                    {
+                    Logger.Log(Logger.Severity.Info, Logger.Framework.Network, $"{ex.Message}");
+                    }
+                }
+
+            } 
+
+            return false;
         }
 
         public static ApiController Instance
