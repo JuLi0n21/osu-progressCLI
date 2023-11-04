3bd55255a4e5ae6e220aa1d44415016954cca55e
JuLi0n21 <julianschlueterbusiness21@gmail.com>
2023-11-04 09:46:00 +0100

    Scoreimporter WIP

---
+++ osu!progressCLI/ScoreImporter.cs
@@ -0,0 +1,118 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using osu1progressbar.Game.Database;
+using System.Globalization;
+
+    namespace osu_progressCLI
+    {
+    internal class ScoreImporter
+    {
+        public async static Task<bool> ImportScores(string filepath) {
+
+            //filepath = @"upload/score.csv";
+            Logger.Log(Logger.Severity.Info, Logger.Framework.Scoreimporter, $"Trying to Parse {filepath}");
+
+            using (var reader = new StreamReader(filepath)) 
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                try
+                {
+                    var records = csv.GetRecords<Score>();
+
+                    // Console.WriteLine(records.Count());
+
+                    foreach (var item in records)
+                    {
+                        Console.WriteLine(item.beatmap_id);
+                        DatabaseController.ImportScore(item.title, item.beatmap_id, item.stars,
+                        item.bpm, item.creator, item.artist, "Ranked", item.diffname,
+                        item.tags, item.set_id, "Pass", item.ar, item.cs, item.hp,
+                        item.od, Credentials.Instance.GetConfig().username, item.accuracy, item.maxcombo, item.score,
+                        item.combo, item.count50, item.count100, item.count300, 0, item.countmiss,
+                        item.mode, item.enabled_mods, item.date_played, item.drain, item.pp, 0, 0, 0, item.rank);
+                    }
+
+                    
+
+                    Logger.Log(Logger.Severity.Info, Logger.Framework.Scoreimporter, $"Scores Successfully imported!");
+
+                }
+                catch (Exception ex)
+                {
+                    Logger.Log(Logger.Severity.Error, Logger.Framework.Scoreimporter, $"{ex.Message}");
+                    return false;
+                }
+            }
+                return true;
+        }
+    }
+
+    public class Score
+    {
+        public int user_id { get; set; }
+        public int beatmap_id { get; set; }
+        public int score { get; set; }
+        public int count300 { get; set; }
+        public int count100 { get; set; }
+        public int count50 { get; set; }
+        public int countmiss { get; set; }
+        public int combo { get; set; }
+        public bool perfect { get; set; }
+        public int enabled_mods { get; set; }
+        public DateTime date_played { get; set; }
+        public string rank { get; set; }
+        public double pp { get; set; }
+        public bool replay_available { get; set; }
+        public double accuracy { get; set; }
+        public int approved { get; set; }
+        public DateTime submit_date { get; set; }
+        public DateTime approved_date { get; set; }
+        public DateTime last_update { get; set; }
+        public string artist { get; set; }
+        public int set_id { get; set; }
+        public double bpm { get; set; }
+        public string creator { get; set; }
+        public int creator_id { get; set; }
+        public double stars { get; set; }
+        public double diff_aim { get; set; }
+        public double diff_speed { get; set; }
+        public double cs { get; set; }
+        public double od { get; set; }
+        public double ar { get; set; }
+        public double hp { get; set; }
+        public int drain { get; set; }
+        public string source { get; set; }
+        public string genre { get; set; }
+        public string language { get; set; }
+        public string title { get; set; }
+        public int length { get; set; }
+        public string diffname { get; set; }
+        public string file_md5 { get; set; }
+        public int mode { get; set; }
+        public string tags { get; set; }
+        public int favorites { get; set; }
+        public double rating { get; set; }
+        public int playcount { get; set; }
+        public int passcount { get; set; }
+        public int circles { get; set; }
+        public int sliders { get; set; }
+        public int spinners { get; set; }
+        public int maxcombo { get; set; }
+        public int storyboard { get; set; }
+        public int video { get; set; }
+        public int download_unavailable { get; set; }
+        public int audio_unavailable { get; set; }
+        public double star_rating { get; set; }
+        public double aim_diff { get; set; }
+        public double speed_diff { get; set; }
+        public double fl_diff { get; set; }
+        public double slider_factor { get; set; }
+        public double speed_note_count { get; set; }
+        public double modded_od { get; set; }
+        public double modded_ar { get; set; }
+        public double modded_cs { get; set; }
+        public double modded_hp { get; set; }
+        public string packs { get; set; }
+    }
+
+}
