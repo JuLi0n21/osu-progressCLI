2bfa8451cacc61a3cb335483dbc8cb75853de7f0
JuLi0n21 <julianschlueterbusiness21@gmail.com>
2024-02-21 00:28:59 +0100

    Base For Auto Updater

--- osu!progressCLI/Program.cs
+++ osu!progressCLI/Program.cs
@@ -4,11 +4,22 @@ using osu1progressbar.Game.MemoryProvider;
 using osu_progressCLI;
 using osu_progressCLI.Datatypes;
 using osu_progressCLI.Webserver.Server;
+using Velopack;
+using Fleck;
+using Velopack.Sources;
 
 class Program
 {
     static async Task Main(string[] args)
     {
+        VelopackApp.Build().WithRestarted((v) => { 
+            // Move Config/Db/public to Parent Dir 
+        }).WithFirstRun((v) =>
+        {
+            // Move Config/Db/public to Parent Dir
+        }).Run();
+        
+
         Logger.SetConsoleLogLevel(Logger.Severity.Warning);
         if (args.Length > 0)
         {
@@ -19,15 +30,24 @@ class Program
                     Logger.SetConsoleLogLevel(Logger.Severity.Debug);
                 }
             }
+        } else
+        {   try
+            {
+                await UpdateMyApp();
+            } catch (Exception e) {
+                Console.WriteLine($"Automatic Updating Failed: {e.Message}");
+            }
+            
         }
 
-        Task.Run(() => ScoreImporter.Instance.StartImporting());
-
-        Console.WriteLine("Welcome to osu!progress");
+        Console.WriteLine($"Welcome to osu!progress");
         Console.WriteLine("If this is ur first time running read the README.txt");
         Console.WriteLine(
             "Keep this Terminal Open or the Progamm will stop if u want it run in the background follow the guide on the github!"
         );
+
+        Task.Run(() => ScoreImporter.Instance.StartImporting());
+
         Logger.Log(Logger.Severity.Info, Logger.Framework.Misc, "Initialzing Components");
 
         OsuMemoryProvider memoryProvider = new OsuMemoryProvider("osu!");
@@ -35,14 +55,21 @@ class Program
         memoryProvider.Run();
         memoryProvider.ReadDelay = 1;
 
-        JArray array = await ApiController.Instance.getTopScores();
-
-        DatabaseController databaseController = new DatabaseController();
-        List<Score> scores = databaseController.GetPotentcialtopplays(250);
-
-        //Console.WriteLine($"{job.ToString()}");
         await Webserver.Instance().start();
 
         memoryProvider.Stop();
+    }
+
+    private static async Task UpdateMyApp()
+    {
+        var mgr = new UpdateManager(new GithubSource("https://github.com/JuLi0n21/autoupdatetest", null, false));    
+
+        var newVersion = await mgr.CheckForUpdatesAsync();
+        if (newVersion == null)
+            return; 
+
+        await mgr.DownloadUpdatesAsync(newVersion);
+
+        mgr.ApplyUpdatesAndRestart(newVersion);
     }
 }
